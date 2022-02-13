27d595736555f611b6e7aa3c70902b28a1f896e3
lutrak.developer <lutrak.developer@gmail.com>
2022-02-13 21:34:00 +0200

    Added some images, widgets and flutter_svg.

---
+++ lib/presentation/core/widgets/other/logo.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class Logo extends StatelessWidget {
+  const Logo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SvgPicture.asset(
+      "assets/images/logo.svg",
+      width: 100,
+    );
+  }
+}
--- lib/presentation/launch/pages/launch_widget.dart
+++ lib/presentation/launch/pages/launch_widget.dart
@@ -1,53 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:pizza_app/application/core/notifiers/theme/multi_text_theme_notifier.dart';
-import 'package:pizza_app/presentation/core/icons/pizza_icons.dart';
-import 'package:pizza_app/presentation/core/widgets/other/gap.dart';
-import 'package:pizza_app/presentation/core/widgets/other/none.dart';
-import 'package:provider/provider.dart';
+import 'package:pizza_app/presentation/core/widgets/other/logo.dart';
 
 class LaunchPage extends StatelessWidget {
   const LaunchPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            const _LaunchText(),
-            const Gap(size: 10),
-            const Icon(PizzaIcons.location),
-            SizedBox(
-              width: 150,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  ColoredBox(
-                    color: Colors.red,
-                    child: SizedBox.fromSize(
-                      size: const Size.fromRadius(10),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class _LaunchText extends StatelessWidget {
-  const _LaunchText({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Text(
-      "wow",
-      style: context.select<MultiTextThemeNotifier, TextStyle>(
-        (value) => value.playFair.bodyText1!,
+        child: Logo(),
       ),
     );
   }
